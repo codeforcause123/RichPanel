6479f58053e6b98b6de525b7bb18c9630fd2fe3c
Aayush <aayush.130702@gmail.com>
2023-08-05 22:52:30 +0530

    'fix'

---
+++ backend/stripe.js
@@ -0,0 +1,55 @@
+const Stripe = require("stripe")
+const stripe = Stripe("sk_test_51NbcodSBuYqweobIsyV0uhT1AzwvERZfaE4ihAJPaZICSLn6jgXUBdxDhNUDRqSDdPhRX3dO9WCBNc9VajRnZW4n00Z1hd4y2c");
+
+const addNewCustomer = async (email, desc, name) => {
+    const customer = await stripe.customers.create({
+        email: email,
+        name: name,
+        description: desc,
+    });
+    return customer;
+}
+
+const createCheckoutSession = async (customer, priceID) => {
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        customer: customer,
+        line_items: [
+            {
+                price: priceID,
+                quantity: 1,
+            },
+        ],
+        mode: 'subscription',
+        success_url: 'http://localhost:3000/success',
+        cancel_url: 'http://localhost:3000/failure',
+    });
+    return session;
+}
+
+const getCustomer = async (customerID) => {
+    const customer = await stripe.customers.retrieve(customerID);
+    return customer;
+}
+
+const cancelSubscription = async (custID) => {
+    const subscriptions = await stripe.subscriptions.list({
+        customer: custID,
+    });
+    try {
+        const subID = subscriptions["data"][0]["id"];
+        console.log(subID);
+        const session = await stripe.subscriptions.del(subID);
+        return session;
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+module.exports = {
+    addNewCustomer,
+    createCheckoutSession,
+    getCustomer,
+    cancelSubscription
+}
---
+++ frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { UserAuth } from '../../context/AuthContext';
+import {
+    Nav,
+    NavLink,
+    Bars,
+    NavMenu,
+    NavBtn,
+    NavBtnLink,
+} from '../../styles/styles';
+
+const Navbar = () => {
+    const { user, logOut } = UserAuth();
+    return (
+        <>
+            <Nav>
+                <Bars />
+
+                <NavMenu>
+                    <NavLink to='/login' activeStyle>
+                        Login
+                    </NavLink>
+                    <NavLink to='/billing' activeStyle>
+                        Billing
+                    </NavLink>
+                    <NavLink to='/' activeStyle>
+                        SignUp
+                    </NavLink>
+
+                </NavMenu>
+
+                <button onClick={async () => { await logOut() }}>Log Out</button>
+
+            </Nav>
+
+        </>
+    );
+};
+
+export default Navbar;
